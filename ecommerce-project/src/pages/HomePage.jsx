0ea8da8c1e717e1de60ad264646be0333b8064c1
Jason Dagger <jason.dagger@nerdninjas.com>
2025-08-30 17:12:36 -0700

    fetch data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { products } from '../assets/products.js'
 import './HomePage.css'
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+        });
+
 return (
     <>
         <title>ECommerce Project</title>
